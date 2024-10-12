70d256c67e65ba50dd603d7ea2e7e7eb0fc03de4
UNAPIMD <ExpIgn@gmail.com>
2024-10-12 16:41:22 +0300

    Remove method ChangeIsMoving

--- Mechanics/Assets/Scripts/IMoving.cs
+++ Mechanics/Assets/Scripts/IMoving.cs
@@ -12,17 +12,9 @@ public interface IMoving
     /// <summary>
     /// Скорость
     /// </summary>
-    public float Speed { get; }
+    public float WalkSpeed { get; }
     /// <summary>
     /// Находится ли объект в состоянии движения?
     /// </summary>
     public bool IsMoving { get; }
-    /// <summary>
-    /// Перемещение объекта
-    /// </summary>
-    void Move();
-    /// <summary>
-    /// Изменяет состояние объекта
-    /// </summary>
-    void ChangeIsMoving();
 }
