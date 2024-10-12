d5de04f309cef4c56a5f5d3d1f9525df42a8974b
UNAPIMD <ExpIgn@gmail.com>
2024-10-12 16:43:12 +0300

    Add Boost for Player

--- Mechanics/Assets/Scripts/Player.cs
+++ Mechanics/Assets/Scripts/Player.cs
@@ -4,30 +4,52 @@ using UnityEngine;
 /// <summary>
 /// Игрок
 /// </summary>
-public class Player : MonoBehaviour, IMoving
+public class Player : MonoBehaviour, IMoving, IRunning
 {
     /// <summary>
     /// Скорость
     /// </summary>
-    [SerializeField] private float _speed;
-    public float Speed
+    [SerializeField] private float _walkSpeed;
+    public float WalkSpeed
     {
-        get => _speed; private set
+        get => _walkSpeed; private set
         {
             if (value < 0f) throw new ArgumentOutOfRangeException("Player: Speed < 0");
-            _speed = value;
+            _walkSpeed = value;
         }
     }
     /// <summary>
     /// Находится ли объект в состоянии движения?
     /// </summary>
-    [SerializeField] private bool _isMoving;
+    private bool _isMoving;
     public bool IsMoving { get => _isMoving; private set => _isMoving = value; }
     /// <summary>
     /// Каркас
     /// </summary>
     private Transform _trans;
     public Transform Trans { get => _trans; private set => _trans = value; }
+    /// <summary>
+    /// Бежит ли объект
+    /// </summary>
+    private bool _isRunning;
+    public bool IsRunning
+    {
+        get => _isRunning;
+        private set => _isRunning = value;
+    }
+    /// <summary>
+    /// Буст к скорости
+    /// </summary>
+    [SerializeField] private float _runSpeed;
+    public float RunSpeed
+    {
+        get => _runSpeed;
+        private set
+        {
+            if (value < 0f) throw new ArgumentOutOfRangeException("Player: Boost < 0");
+            _runSpeed = value;
+        }
+    }
     /// <summary>
     /// Перемещение игрока посредством клавиатуры
     /// </summary>
@@ -37,6 +59,7 @@ public class Player : MonoBehaviour, IMoving
     }
     void Update()
     {
+        CheckBoost();
         Move();
     }
     /// <summary>
@@ -47,27 +70,39 @@ public class Player : MonoBehaviour, IMoving
         IsMoving = false;
         if (Input.GetKey(KeyCode.A))
         {
-            Trans.position += Vector3.left * Speed * Time.deltaTime;
+            Trans.position += Vector3.left * Speed() * Time.deltaTime;
             IsMoving = true;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            Trans.position += Vector3.right * Speed * Time.deltaTime;
+            Trans.position += Vector3.right * Speed() * Time.deltaTime;
             IsMoving = true;
         }
         if (Input.GetKey(KeyCode.W))
         {
-            Trans.position += Vector3.up * Speed * Time.deltaTime;
+            Trans.position += Vector3.up * Speed() * Time.deltaTime;
             IsMoving = true;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            Trans.position += Vector3.down * Speed * Time.deltaTime;
+            Trans.position += Vector3.down * Speed() * Time.deltaTime;
             IsMoving = true;
         }
     }
     /// <summary>
-    /// Изменяет состояние игрока
+    /// Проверяет, ускорен объект или нет.
     /// </summary>
-    public void ChangeIsMoving() => IsMoving = !IsMoving;
+    void CheckBoost()
+    {
+        if (Input.GetKey(KeyCode.LeftShift)) IsRunning = true;
+        else IsRunning = false;
+    }
+    /// <summary>
+    /// Возвращает конечную скорость игрока
+    /// </summary>
+    float Speed()
+    {
+        if (IsRunning) return RunSpeed;
+        return WalkSpeed;
+    }
 }
