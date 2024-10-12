9298d7614c0ce552f432bb2b756e9684fd1dea2f
UNAPIMD <ExpIgn@gmail.com>
2024-10-12 16:41:07 +0300

    Remove method ChangeIsRunning

--- Mechanics/Assets/Scripts/IRunning.cs
+++ Mechanics/Assets/Scripts/IRunning.cs
@@ -10,17 +10,9 @@ public interface IRunning
     /// <summary>
     /// Буст к скорости
     /// </summary>
-    float Boost { get; }
+    float RunSpeed { get; }
     /// <summary>
     /// Бежит ли объект
     /// </summary>
     bool IsRunning { get; }
-    /// <summary>
-    /// Ускорение
-    /// </summary>
-    void SpeedBoost();
-    /// <summary>
-    /// Изменяет состояние объекта
-    /// </summary>
-    void ChangeIsRunning();
 }
